9b77ccd49586afc0a10c5d7dead71473cb2c9906
bissanmu <bissanmu@gmail.com>
2017-09-30 15:24:24 +0900

    test

--- NotiTest/ViewController.swift
+++ NotiTest/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    @IBAction func pressBtn(_ sender: Any) {
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "This is title : seonwoo"
+        content.subtitle = "This is subtitle : UserNotifications tutoral"
+        content.body = "This is body : 블로그 글 쓰기"
+        content.badge = 1
+        
+        let TimeIntervalTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        
+        
+        let request = UNNotificationRequest(identifier: "timer", content: content, trigger: TimeIntervalTrigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, Error in})
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +40,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
